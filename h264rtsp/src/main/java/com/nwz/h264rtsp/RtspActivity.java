4bd3a97f24485a70913589901bae7ce377fb5cac
nwz <nwz88348307@163.com>
2019-01-22 13:27:28 +0800

    init

---
+++ h264rtsp/src/main/java/com/nwz/h264rtsp/RtspActivity.java
@@ -0,0 +1,43 @@
+package com.nwz.h264rtsp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.gl.SurfaceView;
+import net.majorkernelpanic.streaming.rtsp.RtspServer;
+import net.majorkernelpanic.streaming.video.VideoQuality;
+
+public class RtspActivity extends AppCompatActivity {
+
+    private SurfaceView mSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rtsp);
+
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+
+        // Sets the port of the RTSP server to 1234
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.putString(RtspServer.KEY_PORT, String.valueOf(1234));
+        editor.commit();
+
+        // Configures the SessionBuilder
+        SessionBuilder.getInstance()
+                .setSurfaceView(mSurfaceView)
+                .setPreviewOrientation(90)
+                .setContext(getApplicationContext())
+                .setAudioEncoder(SessionBuilder.AUDIO_NONE)
+                .setVideoEncoder(SessionBuilder.VIDEO_H264)
+                .setVideoQuality(new VideoQuality(800,480,15,500000));
+
+        // Starts the RTSP server
+        this.startService(new Intent(this, RtspServer.class));
+    }
+}
